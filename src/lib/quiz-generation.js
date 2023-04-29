65839132795733e5930441f497b8a5852c8bf8aa
Abdulramon Jemil <80219104+abdulramonjemil@users.noreply.github.com>
2023-04-29 19:01:56 +0100

    Enable api route for generating quiz

---
+++ src/lib/quiz-generation.js
@@ -0,0 +1,109 @@
+import { countWords } from "./counter"
+
+export const QUIZ_GENERATION_PARAMS = {
+  LESSON_ID: "lesson_id",
+  MAX_QUESTIONS_COUNT: "max_questions_count"
+}
+
+export const GENERATION_LIMITS = {
+  MIN_MAX_QUESTIONS_COUNT: 3,
+  MAX_MAX_QUESTIONS_COUNT: 20
+}
+
+export const MAX_WORDS_PER_LESSON_PROMPT_CHUNK = 1200
+export const LESSON_CONTENT_SPLITTER_IN_CHUNKS = "\n\n"
+
+export function toUsablePromptChunks(content) {
+  if (typeof content !== "string")
+    throw new TypeError("'content' must be a string")
+
+  const contentChunks = content.split(LESSON_CONTENT_SPLITTER_IN_CHUNKS)
+  const contentChunksWordCounts = contentChunks.map((chunk) =>
+    countWords(chunk)
+  )
+
+  let lastSummedWordCount = 0
+  const usablePromptChunks = contentChunks.reduce(
+    (promptChunksArray, currentValue, index) => {
+      if (index === 0) {
+        promptChunksArray.push(currentValue)
+        // eslint-disable-next-line prefer-destructuring
+        lastSummedWordCount = contentChunksWordCounts[0]
+        return promptChunksArray
+      }
+
+      const wordCountOfCurrentValue = contentChunksWordCounts[index]
+      const newCombinedWordCount = lastSummedWordCount + wordCountOfCurrentValue
+
+      if (newCombinedWordCount <= MAX_WORDS_PER_LESSON_PROMPT_CHUNK) {
+        promptChunksArray[
+          promptChunksArray.length - 1
+        ] += `${LESSON_CONTENT_SPLITTER_IN_CHUNKS}${currentValue}`
+        lastSummedWordCount = newCombinedWordCount
+      } else {
+        promptChunksArray.push(currentValue)
+        lastSummedWordCount = wordCountOfCurrentValue
+      }
+
+      return promptChunksArray
+    },
+    []
+  )
+
+  return usablePromptChunks
+}
+
+/**
+ * This validates the results returned by API calls to MindsDB. The result is
+ * expected to be JSON and abide by the format defined in the prompt (located at
+ * /src/quiz-gen-prompt.txt).
+ */
+export function isValidQuizDetails(resultString) {
+  if (typeof resultString !== "string") return false
+
+  let parsedJSON = null
+  let errorOccured = false
+
+  try {
+    parsedJSON = JSON.parse(resultString)
+  } catch (error) {
+    errorOccured = true
+  }
+
+  if (errorOccured) return false
+  if (typeof parsedJSON !== "object" || parsedJSON === null) return false
+
+  const quizDetailsArray = parsedJSON.quizDetails
+
+  if (!Array.isArray(quizDetailsArray)) return false
+  if (quizDetailsArray.length === 0) return false
+
+  function isValidQuestionObject(value) {
+    if (typeof value !== "object" || value === null) return false
+    const { question, options, answer, explanation } = value
+
+    function isNonEmptyString(val) {
+      return typeof val === "string" && val !== ""
+    }
+
+    if (!isNonEmptyString(question)) return false
+    if (typeof options !== "object" || options === null) return false
+
+    const { A: optionA, B: optionB, C: optionC, D: optionD } = options
+    if (
+      !isNonEmptyString(optionA) ||
+      !isNonEmptyString(optionB) ||
+      !isNonEmptyString(optionC) ||
+      !isNonEmptyString(optionD)
+    ) {
+      return false
+    }
+
+    if (!["A", "B", "C", "D"].includes(answer)) return false
+    if (!isNonEmptyString(explanation) && explanation !== null) return false
+    return true
+  }
+
+  if (!quizDetailsArray.every(isValidQuestionObject)) return false
+  return true
+}
---
+++ src/pages/api/generate-quiz.js
@@ -0,0 +1,169 @@
+import { withSSRContext } from "aws-amplify"
+import MindsDB from "mindsdb-js-sdk"
+import mysql from "mysql"
+
+import * as queries from "../../graphql/queries"
+import {
+  GENERATION_LIMITS,
+  isValidQuizDetails,
+  QUIZ_GENERATION_PARAMS,
+  toUsablePromptChunks
+} from "../../lib/quiz-generation"
+
+export default async function handler(req, res) {
+  const SSR = withSSRContext({ req })
+
+  if (req.method !== "GET") {
+    res.status(405).end()
+    return
+  }
+
+  let userIsAuthenticated = false
+  try {
+    await await SSR.Auth.currentAuthenticatedUser()
+    userIsAuthenticated = true
+  } catch (error) {
+    userIsAuthenticated = false
+  }
+
+  if (!userIsAuthenticated) {
+    res.status(401).end()
+    return
+  }
+
+  const {
+    [QUIZ_GENERATION_PARAMS.LESSON_ID]: lessonId,
+    [QUIZ_GENERATION_PARAMS.MAX_QUESTIONS_COUNT]: maxQuestionsCountStr
+  } = req.query
+
+  if (typeof lessonId !== "string" || lessonId === "") {
+    res
+      .status(400)
+      .end(`Invalid '${QUIZ_GENERATION_PARAMS.LESSON_ID}' query field`)
+    return
+  }
+
+  const maxQuestionsCount = Number(maxQuestionsCountStr)
+  if (
+    typeof maxQuestionsCount !== "number" ||
+    !Number.isInteger(maxQuestionsCount) ||
+    maxQuestionsCount < GENERATION_LIMITS.MIN_MAX_QUESTIONS_COUNT ||
+    maxQuestionsCount > GENERATION_LIMITS.MAX_MAX_QUESTIONS_COUNT
+  ) {
+    res
+      .status(400)
+      .end(
+        `Invalid '${QUIZ_GENERATION_PARAMS.MAX_QUESTIONS_COUNT}' query field`
+      )
+    return
+  }
+
+  let lessonContent = null
+  let errorOccuredWhileLoadingLesson = false
+
+  try {
+    const lessonFetchingResult = await SSR.API.graphql({
+      query: queries.getLesson,
+      variables: { id: lessonId }
+    })
+
+    const lessonContentToUse = lessonFetchingResult.data.getLesson.content
+    if (typeof lessonContentToUse !== "string" || lessonContentToUse === "")
+      throw lessonContentToUse
+    lessonContent = lessonContentToUse
+  } catch (error) {
+    errorOccuredWhileLoadingLesson = true
+  }
+
+  if (errorOccuredWhileLoadingLesson) {
+    res.status(500).end()
+    return
+  }
+
+  const chunksToUseInPrompts = toUsablePromptChunks(lessonContent)
+
+  let questionsCountPerPrompt = 0
+  let numberOfPromptChunksToUse = chunksToUseInPrompts.length
+
+  if (maxQuestionsCount > chunksToUseInPrompts.length) {
+    questionsCountPerPrompt = Math.floor(
+      maxQuestionsCount / chunksToUseInPrompts.length
+    )
+  } else {
+    questionsCountPerPrompt = 1
+    numberOfPromptChunksToUse = maxQuestionsCount
+  }
+
+  let errorOccuredWhileConnectingWithMindsDB = false
+
+  // See the CREATE MODEL STATEMENT in /src/quiz-gen-prompt.txt and sample in
+  // /src/quiz-gen-sample.txt
+  const quizGenerationQueriesAlt = chunksToUseInPrompts.map(
+    (chunk) =>
+      `SELECT lesson_quiz FROM mindsdb.lesson_quiz_generator WHERE questions_count = ${questionsCountPerPrompt} AND lesson_content = ${mysql.escape(
+        chunk
+      )}`
+  )
+  const quizGenerationQueries = quizGenerationQueriesAlt.slice(
+    0,
+    numberOfPromptChunksToUse
+  )
+
+  try {
+    await MindsDB.connect({
+      user: process.env.MINDSDB_CLOUD_USERNAME,
+      password: process.env.MINDSDB_CLOUD_PASSWORD
+    })
+  } catch (error) {
+    errorOccuredWhileConnectingWithMindsDB = true
+  }
+
+  if (errorOccuredWhileConnectingWithMindsDB) {
+    res.status(500).end()
+    return
+  }
+
+  let errorOccuredWhileRunningQuery = false
+  let singleQuizDetailsObjectToUse = null
+
+  try {
+    const queryResults = await Promise.all(
+      quizGenerationQueries.map((query) => MindsDB.SQL.runQuery(query))
+    )
+
+    const someQueriesCompletedBadly = queryResults.some(
+      (result) => result.rows.length < 1
+    )
+    if (someQueriesCompletedBadly) throw queryResults
+
+    // Only one row of data is expected from the query and it should have the
+    // lesson quiz entry
+    const quizDetailsStringsArray = queryResults.map(
+      (result) => result.rows[0].lesson_quiz
+    )
+    if (!quizDetailsStringsArray.every(isValidQuizDetails)) throw queryResults
+
+    const quizDetailsObjectsArray = quizDetailsStringsArray.map(
+      (detailsString) => JSON.parse(detailsString)
+    )
+
+    const finalQuizDetailsArray = quizDetailsObjectsArray.reduce(
+      (questionsArray, currentValue) => [
+        ...questionsArray,
+        ...currentValue.quizDetails
+      ],
+      []
+    )
+
+    singleQuizDetailsObjectToUse = { quizDetails: finalQuizDetailsArray }
+  } catch (error) {
+    errorOccuredWhileRunningQuery = true
+  }
+
+  if (errorOccuredWhileRunningQuery) {
+    res.status(500).end()
+    return
+  }
+
+  res.status(200).json(singleQuizDetailsObjectToUse)
+}
