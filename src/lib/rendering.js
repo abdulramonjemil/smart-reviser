c91bcfc8068e3332824f1d9a2a4fb58d3d7aaabd
Abdulramon Jemil <80219104+abdulramonjemil@users.noreply.github.com>
2023-05-07 11:26:38 +0100

    Modify and add description to utility function

--- src/lib/rendering.js
+++ src/lib/rendering.js
@@ -1,3 +1,9 @@
+/**
+ * This function enables throttling callbacks with the built in
+ * requestAnimationFrame method. It makes sure the callback isn't actually
+ * called until the browser is ready for a paint.
+ */
+
 // eslint-disable-next-line import/prefer-default-export
 export function throttleWithAnimationFrame(func, reducer = null) {
   if (typeof func !== "function")
@@ -12,20 +18,30 @@ export function throttleWithAnimationFrame(func, reducer = null) {
 
   let thereAreUnrenderedFrames = false
 
+  if (reducer === null) {
+    return function throttledFunction(...params) {
+      if (thereAreUnrenderedFrames) return
+      thereAreUnrenderedFrames = true
+
+      window.requestAnimationFrame(() => {
+        func.call(this, ...params)
+        thereAreUnrenderedFrames = false
+      })
+    }
+  }
+
   return function throttledFunction(...params) {
     if (thereAreUnrenderedFrames) return
 
     // Params to pass is determined here because the function passed to
-    // requestAnimationFrame is called asynchronously. The params to pass to the
+    // requestAnimationFrame is called asynchronously and the params to pass to the
     // real function should be detemined synchronously
-    const reducerReturnValue =
-      reducer !== null ? reducer.call(null, ...params) : null
+    const paramToPass = reducer.call(null, ...params)
+    thereAreUnrenderedFrames = true
 
     window.requestAnimationFrame(() => {
-      if (reducer !== null) func.call(this, reducerReturnValue)
-      else func.call(this, ...params)
+      func.call(this, paramToPass)
       thereAreUnrenderedFrames = false
     })
-    thereAreUnrenderedFrames = true
   }
 }
